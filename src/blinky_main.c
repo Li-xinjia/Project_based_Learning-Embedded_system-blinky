87c1bebfbc8ac74c999597b70f214a897ee99a55
Li-xinjia <xinjia0104@gmail.com>
2022-11-13 18:32:26 +0900

    task 2 over

--- src/blinky_main.c
+++ src/blinky_main.c
@@ -33,6 +33,8 @@
 uint32_t base_led_color;
 uint32_t led_color = LED_ALL;
 
+bool led_on = false;
+
 //*****************************************************************************
 //
 // This function sets up UART0 to be used for a console to display information
@@ -40,24 +42,36 @@ uint32_t led_color = LED_ALL;
 //
 //*****************************************************************************
 void initConsole(void) {
-  // Enable GPIO port A which is used for UART0 pins.
-  SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
-  // Configure the pin muxing for UART0 functions on port A0 and A1.
-  // This step is not necessary if your part does not support pin muxing.
-  GPIOPinConfigure(GPIO_PA0_U0RX);
-  GPIOPinConfigure(GPIO_PA1_U0TX);
-  // Enable UART0 so that we can configure the clock.
-  SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
-  // Use the internal 16MHz oscillator as the UART clock source.
-  UARTClockSourceSet(UART0_BASE, UART_CLOCK_PIOSC);
-  // Select the alternate (UART) function for these pins.
-  GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
-  // Initialize the UART for console I/O.
-  UARTStdioConfig(0, 9600, 16000000);
+    // Enable GPIO port A which is used for UART0 pins.
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
+    // Configure the pin muxing for UART0 functions on port A0 and A1.
+    // This step is not necessary if your part does not support pin muxing.
+    GPIOPinConfigure(GPIO_PA0_U0RX);
+    GPIOPinConfigure(GPIO_PA1_U0TX);
+    // Enable UART0 so that we can configure the clock.
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
+    // Use the internal 16MHz oscillator as the UART clock source.
+    UARTClockSourceSet(UART0_BASE, UART_CLOCK_PIOSC);
+    // Select the alternate (UART) function for these pins.
+    GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
+    // Initialize the UART for console I/O.
+    UARTStdioConfig(0, 9600, 16000000);
 }
 
 void initInterruptPins(void) {
-  // You can write your own code here.
+    // You can write your own code here.
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
+    GPIODirModeSet(GPIO_PORTF_BASE, INT_LEFT_BUTTON, GPIO_DIR_MODE_IN);
+    GPIOPadConfigSet(GPIO_PORTF_BASE, INT_LEFT_BUTTON,
+                     GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD_WPU);
+    GPIOIntClear(GPIO_PORTF_BASE, INT_LEFT_BUTTON);
+
+    GPIOIntRegister(GPIO_PORTF_BASE, SW1PinIntHandler);
+    GPIOIntTypeSet(GPIO_PORTF_BASE, INT_LEFT_BUTTON,
+                   GPIO_FALLING_EDGE);
+    GPIOIntEnable(GPIO_PORTF_BASE, INT_LEFT_BUTTON);
+
+    UARTprintf("Interrupt pins initiate over\n");
 }
 
 //*****************************************************************************
@@ -65,48 +79,84 @@ void initInterruptPins(void) {
 //*****************************************************************************
 
 void SysTickIntHandler(void) {
-  static uint32_t tick_count = 0;
-  if (tick_count % 16 == 0) {
-    led_color = ~led_color;
-    tick_count = 0;
-    /*
-     * Note that the 2nd param of GPIOPinWrite denotes pins to be written in bit pattern.
-     * The 3rd param of denotes values to be written in bit pattern.
-     * What really written to GPIO pins is a logical AND of 2nd and 3rd params.
-     */
-    GPIOPinWrite(GPIO_PORTF_BASE,base_led_color,0); // turn off all LEDs
-    GPIOPinWrite(GPIO_PORTF_BASE,base_led_color,led_color);
-  }
-  tick_count++;
+    static uint32_t tick_count = 0;
+    if (tick_count % 16 == 0) {
+        tick_count = 0;
+        /*
+         * Note that the 2nd param of GPIOPinWrite denotes pins to be written in bit pattern.
+         * The 3rd param of denotes values to be written in bit pattern.
+         * What really written to GPIO pins is a logical AND of 2nd and 3rd params.
+         */
+//        if (led_on) {
+//            GPIOPinWrite(GPIO_PORTF_BASE, base_led_color, led_color);
+//        } else {
+//            GPIOPinWrite(GPIO_PORTF_BASE, base_led_color, 0); // turn off all LEDs
+//        }
+//        led_on = !led_on;
+    }
+    tick_count++;
 }
 
 void SW1PinIntHandler(void) {
+    GPIOIntDisable(GPIO_PORTF_BASE, INT_LEFT_BUTTON);
+    GPIOIntClear(GPIO_PORTF_BASE, INT_LEFT_BUTTON);
+
+    if (led_on) {
+
+        switch (led_color) {
+            case LED_BLUE:
+                led_color = LED_RED;
+                break;
+            case LED_RED:
+                led_color = LED_GREEN;
+                break;
+            case LED_GREEN:
+                led_color = LED_WHITE;
+                break;
+            case LED_WHITE:
+                led_color = LED_BLUE;
+                break;
+            default:
+                break;
+        }
+
+
+        GPIOPinWrite(GPIO_PORTF_BASE, base_led_color, led_color);
+    } else {
+        GPIOPinWrite(GPIO_PORTF_BASE, base_led_color, 0); // turn off all LEDs
+    }
+    led_on = !led_on;
+
+    UARTprintf("SW1 pushed\n");
+    GPIOIntEnable(GPIO_PORTF_BASE, INT_LEFT_BUTTON);
 }
 
 int main(void) {
-  // Set the clocking to run directly from the crystal.
-  ROM_SysCtlClockSet(SYSCTL_SYSDIV_1 | SYSCTL_USE_OSC | SYSCTL_OSC_MAIN |
-		     SYSCTL_XTAL_16MHZ);
-  // Set up ports hardware (see periphConf.c)
-  PortFunctionInit();
+    // Set the clocking to run directly from the crystal.
+    ROM_SysCtlClockSet(SYSCTL_SYSDIV_1 | SYSCTL_USE_OSC | SYSCTL_OSC_MAIN |
+                       SYSCTL_XTAL_16MHZ);
+    // Set up ports hardware (see periphConf.c)
+    PortFunctionInit();
 
-  // Set up interrupts (you can specify GPIO interrupt initialization here)
-  initInterruptPins();
+    // Initialize console
+    initConsole();
 
-  // Initialize console
-  initConsole();
+    // Set up interrupts (you can specify GPIO interrupt initialization here)
+    initInterruptPins();
+
 //  UARTprintf("Hello, world!\n");
-  
-  led_color = LED_ALL;
-  base_led_color = LED_BLUE;
-  GPIOIntEnable(GPIO_PORTF_BASE,INT_ALL_BUTTONS);
-  GPIOPinWrite(GPIO_PORTF_BASE,base_led_color,led_color);
 
-  SysTickPeriodSet(SysCtlClockGet() / SYSTICKS_PER_SEC);
-  SysTickEnable();
-  SysTickIntRegister(SysTickIntHandler);
-  SysTickIntEnable();
+    led_color = LED_BLUE;
+    base_led_color = LED_ALL;
 
-  while(1);
+    GPIOIntEnable(GPIO_PORTF_BASE, INT_LEFT_BUTTON);
+    GPIOPinWrite(GPIO_PORTF_BASE, base_led_color, led_color);
+
+    SysTickPeriodSet(SysCtlClockGet() / SYSTICKS_PER_SEC);
+    SysTickEnable();
+    SysTickIntRegister(SysTickIntHandler);
+    SysTickIntEnable();
+
+    while (1);
 }
 
